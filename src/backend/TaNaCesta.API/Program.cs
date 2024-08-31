d435fc011b7f1549733d71aedbc80bfb8ca7044c
Willian Gonçalves <santoswil81@gmail.com>
2024-08-31 11:28:03 -0300

    feat: Adicionando o UserController

--- src/backend/TaNaCesta.API/Program.cs
+++ src/backend/TaNaCesta.API/Program.cs
@@ -1,11 +1,15 @@
+using TaNaCesta.Application.UseCases.User.Register;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
