3436d8b0789535d1979047613dfcfcaa88e750e0
Willian Gonçalves <santoswil81@gmail.com>
2024-09-02 18:11:27 -0300

    feat: Atualizando registros de usuários

--- src/backend/TaNaCesta.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/backend/TaNaCesta.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,26 +1,33 @@
 using TaNaCesta.Communication.Requests;
 using TaNaCesta.Communication.Responses;
-using TaNaCesta.Application.Services;
-using System.Collections.Generic;
 using AutoMapper;
+using TaNaCesta.Domain.Interfaces;
+using TaNaCesta.Domain.Exceptions;
+using TaNaCesta.Application.Services;
 
 namespace TaNaCesta.Application.UseCases.User.Register
 {
     public class RegisterUserUseCase : IRegisterUserUseCase
     {
         private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        private readonly PasswordEncripter _passwordEncripter;
 
-        public RegisterUserUseCase(IMapper mapper)
+        public RegisterUserUseCase(IMapper mapper, IUserRepository userRepository, PasswordEncripter passwordEncripter)
         {
             _mapper = mapper;
+            _userRepository = userRepository;
+            _passwordEncripter = passwordEncripter;
         }
         
         public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
         { 
-            //Validar a request
-            await Validate(request);
+            Validate(request);
             
             var user = _mapper.Map<Domain.Entities.User>(request);
+            user.Password = _passwordEncripter.Encrypt(request.Password);
+            
+            await _userRepository.Add(user);
 
             return new ResponseRegisteredUserJson
             {
@@ -28,17 +35,31 @@ namespace TaNaCesta.Application.UseCases.User.Register
             };
         }
 
-        private async Task Validate(RequestRegisterUserJson request)
+        private void Validate(RequestRegisterUserJson request)
         {
             var validator = new RegisterUserValidator();
 
             var result = validator.Validate(request);
 
+            var emailExists = _userRepository.ExistActiveUserWithEmail(request.Email);
+
+            var cpfExists = _userRepository.ExistActiveUserWithCpf(request.Cpf);
+
+            if (emailExists)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, "E-mail já cadastrado!"));
+            }
+
+            if (cpfExists)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, "CPF já cadastrado!"));
+            }
+
             if (!result.IsValid)
             {
                 var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
 
-                throw new Exception();
+                throw new ErrorOnValidationException(errorMessages);
             }
         }
 
