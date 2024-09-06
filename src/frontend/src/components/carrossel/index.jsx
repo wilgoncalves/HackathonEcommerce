4f1ee8e26a6a8406755bede45bb05af576cb4af4
Vinicius Dyonisio <viniciusdyo@gmail.com>
2024-09-05 21:22:39 -0300

    feat: consumo da api através do redux para listar os produtos

--- src/frontend/src/components/carrossel/index.jsx
+++ src/frontend/src/components/carrossel/index.jsx
@@ -1,31 +1,20 @@
 import PropTypes from "prop-types";
-import { FaPlus, FaArrowRight } from "react-icons/fa";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+//import { useContext } from 'react';
+import { FaArrowRight, FaPlus } from "react-icons/fa";
 import "swiper/css";
 import 'swiper/css/navigation';
-import { useContext } from 'react';
-import { CartContext } from '../../CartContext.jsx';
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+//import { CartContext } from '../../CartContext.jsx';
 
-import { useDispatch } from 'react-redux';
-import * as actions from '../../store/modules/cart/actions.jsx'
 
 const ProductCarousel = ({ title, products }) => {
 
-  const { addToCart } = useContext(CartContext);
+  // const { addToCart } = useContext(CartContext);
 
-  const dispatch = useDispatch();
-
-
-  const handleClick = (e) => {
-      e.preventDefault();
-      const products = [];
-      products.push({ name: "Teste" })
-      if (products.length > 0) {
-          dispatch(actions.cartRequest({ name: "Teste" }));
-      }
-  }
-
+  // const handleClick = (e) => {
+  //   e.preventDefault();
+  // }
   return (
     <div id="produtos" className="product-carousel">
       <div className="section-title flex justify-between items-center">
@@ -58,7 +47,8 @@ const ProductCarousel = ({ title, products }) => {
         modules={[Navigation]}
         className="mySwiper"
       >
-        {products.map(({ name, price, image, description }, index) => (
+        {products.map(({ name, price }, index) => (
+
           <SwiperSlide
             key={index}
             className="border border-gray-200 hover:border-gray-300 rounded-lg p-4"
@@ -66,25 +56,27 @@ const ProductCarousel = ({ title, products }) => {
             <div className="flex flex-row p-7 items-center space-x-6">
               <div className="product-info space-y-3 w-[70%]">
                 <h3 className="font-bold text-xl">{name}</h3>
-                <p className="text-darkFadeColor text-[14px]">{description}</p>
+                <p className="text-darkFadeColor text-[14px]">{name}</p>
                 <hr className="text-darkFadeColor"></hr>
                 <div className="flex items-center space-x-3">
                   <p className="product-price font-bold text-2xl">R${price}</p>
                   <div className="bg-redNormal p-2 rounded-full"
-                  onClick={handleClick}
-                  /*onClick={() =>
-                    addToCart({ name, price, image, description })}*/
+                  // onClick={handleClick}
+                  // onClick={() =>
+                  //   addToCart({ name, price, image, description })}
+
                   >
                     <FaPlus className="text-whiteNormal" />
                   </div>
                 </div>
               </div>
               <div className="product-image">
-                <img src={(`./src/assets/products/${image}`)} alt={name} />
+                <img src={(`./src/assets/products/abobora`)} alt={name} />
               </div>
             </div>
           </SwiperSlide>
         ))}
+
       </Swiper>
     </div>
   );
@@ -95,7 +87,7 @@ ProductCarousel.propTypes = {
   products: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      price: PropTypes.number.isRequired,
+      price: PropTypes.string.isRequired,
       image: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
     })
--- src/frontend/src/pages/home/index.jsx
+++ src/frontend/src/pages/home/index.jsx
@@ -1,39 +1,24 @@
 import { useContext } from 'react';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { CartContext } from '../../CartContext.jsx';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "../../sections/navBar";
-import Header from "../../sections/header";
-import Highlights from "../../sections/destaques";
+import Cart from "../../components/cartButton/cart.jsx";
 import CartButton from "../../components/cartButton/index.jsx";
-import LoginPage from "../../pages/login";
+import CartPage from "../../pages/cart";
 import SignupPage from "../../pages/createUser";
+import LoginPage from "../../pages/login";
 import Catalog from "../../sections/catalog";
 import Contact from "../../sections/contact";
-import CartPage from "../../pages/cart";
-
-import { useEffect, useState } from "react";
-import Cart from "../../components/cartButton/cart.jsx";
-import axios from "../../services/axios";
+import Highlights from "../../sections/destaques";
+import Header from "../../sections/header";
+import NavBar from "../../sections/navBar";
 
 function Home() {
   const { cartItems } = useContext(CartContext);
 
   const itemCount = cartItems.reduce((total, item) => total + item.quantity, 0);
 
-  const [productList, setListProducts] = useState([]);
-
-
-  useEffect(() => {
-    const get = async () => {
-      const response = await axios.get('products');
-      setListProducts(response.data)
-    }
-    get()
-  }, []);
-
   return (
     <div className="flex flex-col w-full justify-center mt-4 items-center">
-      {console.log(productList)}
       <CartButton itemCount={itemCount} />
       <NavBar />
       <Header />
