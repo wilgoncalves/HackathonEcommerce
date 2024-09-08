f6894ebd203515257efc91d8d155bbeee619d253
Andre Silva <hardsys_andre@hotmail.com>
2024-09-08 00:16:06 -0300

    fix: request

--- src/backend/TaNaCesta.Application/obj/Debug/net6.0/TaNaCesta.Application.AssemblyInfo.cs
+++ src/backend/TaNaCesta.Application/obj/Debug/net6.0/TaNaCesta.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaNaCesta.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c3ceaa143eaa426bcbfefcfb9966ea2aedab98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaNaCesta.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaNaCesta.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
