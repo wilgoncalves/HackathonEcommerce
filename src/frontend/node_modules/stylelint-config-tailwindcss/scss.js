e08bb0997add0e79399affe7c1995d23e33f442e
Andre Silva <hardsys_andre@hotmail.com>
2024-09-07 23:37:53 -0300

    fix: remove node_modules

--- src/frontend/node_modules/stylelint-config-tailwindcss/scss.js
+++
@@ -1,8 +0,0 @@
-const { rules } = require('./index');
-
-module.exports = {
-  rules: Object.entries(rules).reduce(
-    (rules, [k, v]) => Object.assign({ [`scss/${k}`]: v }, rules),
-    rules
-  ),
-};
