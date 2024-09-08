72fd8b7b3c3cb432dff4de80012db74b73a86f2c
Vinicius Dyonisio <viniciusdyo@gmail.com>
2024-09-08 01:20:37 -0300

    fix:alterado o  caminho das imagens do produto para o deploy

--- src/frontend/src/components/carrossel/index.jsx
+++ src/frontend/src/components/carrossel/index.jsx
@@ -63,7 +63,7 @@ const ProductCarousel = ({ title, products }) => {
                 </div>
                 <div className="product-image md:w-full w-[80px]">
                   <img
-                    src={`./src/assets/products/${name
+                    src={`./assets/products/${name
                       .split(" ")[0]
                       .toLowerCase()
                       .normalize("NFD")
